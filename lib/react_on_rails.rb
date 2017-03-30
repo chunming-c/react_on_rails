fe987fd57cc306f060de412a2e7b5b3bd2a3841b
Nguyen, Phuc | Hook | ECMD <phuc.nguyen@rakuten.com>
2017-03-30 17:21:13 +0900

    tested

--- lib/react_on_rails.rb
+++ lib/react_on_rails.rb
@@ -1,8 +1,6 @@
 require "rails"
 
 require "react_on_rails/controller"
-require "react_on_rails/version"
-require "react_on_rails/version_checker"
 require "react_on_rails/configuration"
 require "react_on_rails/server_rendering_pool"
 require "react_on_rails/engine"
