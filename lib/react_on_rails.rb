d6e5ab1e3c9df172f2e51cb100a95ade61ac0810
Nguyen, Phuc | Hook | ECMD <phuc.nguyen@rakuten.com>
2017-03-30 17:09:56 +0900

    updated version checker

--- lib/react_on_rails.rb
+++ lib/react_on_rails.rb
@@ -1,7 +1,10 @@
 require "rails"
 
 require "react_on_rails/controller"
+require "react_on_rails/version"
+require "react_on_rails/version_checker"
 require "react_on_rails/configuration"
 require "react_on_rails/server_rendering_pool"
 require "react_on_rails/engine"
+require "react_on_rails/version_syntax_converter"
 require "react_on_rails/utils"
